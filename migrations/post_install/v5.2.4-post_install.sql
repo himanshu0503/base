3725c1fc36524a32b056a1da69893c7897fc5ed7
Bharath Upadhya <1992bharath@gmail.com>
2017-02-20 16:57:59 +0530

    updating the post-install script

--- migrations/post_install/v5.2.4-post_install.sql
+++ migrations/post_install/v5.2.4-post_install.sql
@@ -1,9 +1,10 @@
 do $$
   begin
     if exists (select 1 from information_schema.columns where table_name = 'projects' and column_name = 'isOrphaned') then
-      create temp table temp_pa as select "projectId" as id from "projectAccounts" group by "projectId";
-      update projects set "isOrphaned" = true from (select projects.id from projects left join temp_pa on projects.id = temp_pa.id where temp_pa.id is NULL) T where T.id = projects.id;
-      update projects set "isOrphaned" = false from temp_pa where projects.id = temp_pa.id;
+      update projects set "isOrphaned" = false where "isOrphaned" is NULL;
+      create temp table temp_pa as select id from projects;
+      delete from temp_pa using "projectAccounts" where "projectAccounts"."projectId" = temp_pa.id;
+      update projects set "isOrphaned" = true from temp_pa where temp_pa.id = projects.id;
       drop table temp_pa;
     end if;
   end
