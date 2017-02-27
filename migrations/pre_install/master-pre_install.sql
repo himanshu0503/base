bcc3cab21da99a17b13dc52e4951a4201d980638
Riturajcse <riturajcse1994@gmail.com>
2017-02-27 15:51:54 +0530

    updates query

--- migrations/pre_install/master-pre_install.sql
+++ migrations/pre_install/master-pre_install.sql
@@ -1,7 +1,7 @@
 do $$
   begin
     if exists (select * from "subscriptions" where "systemMachineImageId" is null) then
-      update "subscriptions" set "systemMachineImageId" = (select "id" from "systemMachineImages" where "isDefault"=true);
+      update "subscriptions" set "systemMachineImageId" = (select "id" from "systemMachineImages" where "isDefault"=true) where "systemMachineImageId" is null;
     end if;
   end
 $$;
