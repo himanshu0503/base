0cff802d0d87fcf15f463456c413d7a48c22dcbb
Riturajcse <riturajcse1994@gmail.com>
2017-02-27 16:11:18 +0530

    removes unwanted check

--- migrations/pre_install/master-pre_install.sql
+++ migrations/pre_install/master-pre_install.sql
@@ -1,7 +1,5 @@
 do $$
   begin
-    if exists (select * from "subscriptions" where "systemMachineImageId" is null) then
-      update "subscriptions" set "systemMachineImageId" = (select "id" from "systemMachineImages" where "isDefault"=true) where "systemMachineImageId" is null;
-    end if;
+    update "subscriptions" set "systemMachineImageId" = (select "id" from "systemMachineImages" where "isDefault"=true) where "systemMachineImageId" is null;
   end
 $$;
