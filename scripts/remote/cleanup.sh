714eb2eb30ead69404cab897472926475a63bab2
Shrivara <shrivara.jois@gmail.com>
2017-03-02 10:35:47 +0530

    Use image id to cleanup to ensure dangling images are removed

--- scripts/remote/cleanup.sh
+++ scripts/remote/cleanup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 
 readonly COMPONENT_REPOSITORY=$1
-readonly DEPLOY_VERSION=$2
+readonly RELEASE=$2
 
 remove_old_images() {
   local all_images=$(sudo docker images)
   echo "$all_images" | while read -r line; do
     local image_repository=$(echo $line | awk '{print $1}')
     local image_tag=$(echo $line | awk '{print $2}')
+    local image_id=$(echo $line | awk '{print $3}')
     if [ "$image_repository" == "$COMPONENT_REPOSITORY" ]; then
-      if [ "$image_tag" != "$DEPLOY_VERSION" ]; then
-        echo $image
+      if [ "$image_tag" != "$RELEASE" ]; then
         local stale_image=$image_repository:$image_tag
-        echo "Stale image found, removing : $stale_image"
-        sudo docker rmi $stale_image || true
+        echo "Stale image $stale_image found, attempting to remove..."
+        sudo docker rmi $image_id || true
       fi
     fi
   done
 }
 
 main() {
-  if [ -z "$DEPLOY_VERSION" ]; then
-    echo "DEPLOY_VERSION env required to remove stale images"
+  if [ -z "$RELEASE" ]; then
+    echo "RELEASE env required to remove stale images"
     exit 1
   fi
 
